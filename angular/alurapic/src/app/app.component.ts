72eb30dd2ec000661b0518bb181f28b1fa583e20
CarolineFns <carolineadm.santos@gmail.com>
2020-05-18 20:56:29 -0300

    criando o projeto alurapic

---
+++ angular/alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  url = 'https://cdn.domtotal.com/img/noticias/2019-03/1331528_394055.jpg'
+  description = 'Leão';
+}
