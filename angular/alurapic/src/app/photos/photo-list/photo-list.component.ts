f5920476d97011f1ad5fdecce424b31304684840
CarolineFns <carolineadm.santos@gmail.com>
2020-05-23 20:54:29 -0300

    utilizando resolver para carregamento das fotos

--- angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -13,11 +12,10 @@ export class PhotoListComponent implements OnInit {
   @Input() filter = '';
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService, private activatedRoute: ActivatedRoute){}
+  constructor(private activatedRoute: ActivatedRoute){}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService.listFromUser(userName).subscribe(photos => this.photos=photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
 }
