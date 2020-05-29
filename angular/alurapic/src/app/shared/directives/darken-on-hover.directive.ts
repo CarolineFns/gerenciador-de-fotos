9df59709691ee4b0c30414311fdc2831303620a9
CarolineFns <carolineadm.santos@gmail.com>
2020-05-28 22:01:48 -0300

    criando diretiva darkenOnHover pra aplicar css manipulando elemento do DOM

---
+++ angular/alurapic/src/app/shared/directives/darken-on-hover.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, HostListener, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+    selector: '[apDarkenOnHover]'
+})
+export class DarkenOnHoverDirective {
+    constructor(private el: ElementRef, private render: Renderer2){}
+
+    @Input() brightness = '70%';
+    
+    @HostListener('mouseover')
+    darkenOn(){
+        this
+        .render
+        .setStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+
+    @HostListener('mouseleave')
+    darkenOff(){
+        this
+        .render
+        .setStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    }
+}
