5f754c9d0fd6b6c5f47625796e8f8cf504a235b6
CarolineFns <carolineadm.santos@gmail.com>
2020-06-02 23:10:39 -0300

    Criando o serviço de autenticação e enviando via post para a API

--- angular/alurapic/src/app/home/signin/signin.component.ts
+++ angular/alurapic/src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/auth.service';
+
 @Component({
     templateUrl: './signin.component.html'
 })
@@ -8,7 +10,9 @@ export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService){}
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -16,4 +20,20 @@ export class SignInComponent implements OnInit {
             password: ['', Validators.required]
         });
     }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => console.log('autenticado'),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    alert('Invalid user name or password');
+                }
+            );
+        }
 }
