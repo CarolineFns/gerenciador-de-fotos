9417cd68d6784d84f5330ef0661e918dbc2cdbcb
CarolineFns <carolineadm.santos@gmail.com>
2020-06-07 19:25:33 -0300

    usando o behavioSuject para exibir o valor do nome do usuario no header depois de logar

--- angular/alurapic/src/app/core/header/header.component.ts
+++ angular/alurapic/src/app/core/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
+
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
 
+    user: User;
+
+    constructor(userService: UserService){
+        userService.getUser().subscribe(user => this.user = user);
+    }
 }
