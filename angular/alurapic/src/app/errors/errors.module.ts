93b6bf0ff92e5cd77b2a46b3cc50b0c2d03a4e43
CarolineFns <carolineadm.santos@gmail.com>
2020-08-12 22:57:59 -0300

    classe para lidar com os erros da aplicação usando errorhandle

--- angular/alurapic/src/app/errors/errors.module.ts
+++ angular/alurapic/src/app/errors/errors.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [NotFoundComponent],
-  imports: [CommonModule]
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
+  ]
 })
 export class ErrorsModule {}
